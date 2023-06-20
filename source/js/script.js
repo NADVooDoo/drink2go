cc21d3af1ed4fa41d90845fa7dd1b30ff7e9b2f6
Nikolai Didur <nadvoodoo@gmail.com>
2023-06-20 18:27:06 +0300

    adding mobile adaptive version + slider + map

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,59 @@
-/* в этот файл добавляет скрипты*/
+const menu = () => {
+  let navMain = document.querySelector('.main-nav');
+  let navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+};
+
+menu();
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  grabCursor: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const map = () => {
+  const map = L.map('map').setView([59.9684, 30.3176], 17);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  const iconOptions = {
+    iconUrl: 'img/map-cursor.svg',
+    iconSize: [38, 50],
+  }
+
+  const customIcon = L.icon(iconOptions);
+
+  let markerOptions = {
+    icon: customIcon
+  }
+
+  const marker = L.marker({ lat: 59.9684, lng: 30.3176 }, markerOptions);
+
+  marker.addTo(map);
+}
+
+map();
